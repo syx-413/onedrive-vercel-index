4e40993132671f2f9a01c44de648725b2887247a
LS <the413reply@gmail.com>
2025-02-20 03:34:57 +0000

    update official

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -211,8 +211,9 @@ export default function SearchModal({
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <DialogPanel  className="my-12 inline-block w-full max-w-3xl transform overflow-hidden rounded border border-gray-400/30 text-left shadow-xl transition-all">
-            {/* <DialogPanel className="my-8 inline-block w-full max-w-3xl transform space-y-4"> */}
+            <DialogPanel
+              className="w-full max-w-3xl rounded-xl bg-white/95 dark:bg-gray-900/95 p-6 backdrop-blur-xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+            >
               <DialogTitle
                 as="h3"
                 className="flex items-center space-x-4 border-b border-gray-400/30 bg-gray-50 p-4 dark:bg-gray-800 dark:text-white"
